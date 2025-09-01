17443eac32f4588f19d163cb4bca5b8a9186553e
ClaraMeirelles <clara_meirelles@hotmail.com>
2025-09-01 15:49:12 -0300

    início app - criação da lista e estado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([{ title: "titulo1", id: "1", done: false }, { title: "titulo2", id: "2", done: false }, { title: "titulo3", id: "3", done: false }])
+
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+      <h1>To-Do App</h1>
+      <section>
+        {tasks.map((task) => {
+          return <article key={task.id}>
+            <h3>{task.title}</h3>
+          </article>
+
+        })}
+      </section>
+    </main>
   )
 }
 
