42afc17ab5507e3488f282f010e2b987f8cdd4ed
ClaraMeirelles <clara_meirelles@hotmail.com>
2025-09-05 10:56:30 -0300

    deleção arquivos gerados docker

--- frontend/src/components/TaskList/TaskList.d.ts
+++
@@ -1,2 +0,0 @@
-import type { TaskListProps } from "./TaskList.types";
-export default function TaskList({ tasks, toggleTask, search }: TaskListProps): import("react/jsx-runtime").JSX.Element;
--- frontend/src/components/TaskList/TaskList.types.d.ts
+++
@@ -1,6 +0,0 @@
-import type { Task } from "../../types/Task";
-export interface TaskListProps {
-    tasks: Task[];
-    toggleTask: (id: string) => void;
-    search: string;
-}
